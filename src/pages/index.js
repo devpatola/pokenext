3e048db397445fe30fdf456a8472a427865d5873
Marlon Couto <devpatola@proton.me>
2023-02-25 17:41:03 -0300

    Adiciona dados dos pokemon trazidos pela API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,32 @@
 import styles from "@/styles/Home.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemon = 251;
+  const endpoint = "https://pokeapi.co/api/v2/pokemon/";
+
+  const response = await fetch(`${endpoint}/?limit=${maxPokemon}`);
+  const data = await response.json();
+
+  data.results.forEach((pokemon, index) => {
+    pokemon.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemon: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemon }) {
   return (
-    <>
+    <div>
       <h1>PokeNext</h1>
-    </>
+      <ul>
+        {pokemon.map(({ name, id }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
