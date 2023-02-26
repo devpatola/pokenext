f0c227df7ed65bd58d5299edcdee004d24f58d8b
Marlon Couto <devpatola@proton.me>
2023-02-25 21:15:20 -0300

    Adiciona estilização à pagina de detalhes do pokemon

--- src/pages/pokemon/[pokemonId].js
+++ src/pages/pokemon/[pokemonId].js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import styles from "@/styles/PokemonId.module.css";
 
 export async function getStaticPaths() {
   const maxPokemon = 251;
@@ -36,8 +38,41 @@ export async function getStaticProps(context) {
 
 export default function PokemonId({ pokemon }) {
   return (
-    <>
-      <p>{pokemon.name}</p>
-    </>
+    <div className={styles.pokemon_container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+        width="200"
+        height="200"
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map((type, index) => (
+            <span
+              key={index}
+              className={`${styles.type} ${styles[`type_${type.type.name}`]}`}
+            >
+              {type.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className={styles.data_container}>
+        <div className={styles.data_height}>
+          <h4>Altura:</h4>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+        <div className={styles.data_weight}>
+          <h4>Peso:</h4>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
   );
 }
